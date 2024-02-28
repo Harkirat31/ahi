fcdf2ee265e2379a60bc39d5ca7606bb1d36b11d
AHI WEBADMIN <ahiwebadmin@ANDRECAN.local>
2024-02-28 15:55:31 -0500

    Impl Home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,8 @@
+import Image from 'next/image'
 const home =()=>{
-    return <div className="bg-slate-500">HOME</div>
+    return <div className="bg-cover h-96 bg-[url('/dummy/bg5.jpg')]" >
+        
+    </div>
 }
 
 export default home
